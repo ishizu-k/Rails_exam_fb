52e07ce02dae02db3d320508bb9b6a4fc91ad5dc
ishizu <demekinn_96@yahoo.co.jp>
2019-06-04 12:19:01 +0900

    ログインに失敗した時の処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログインが成功した時
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render 'new'
+    end
+  end
+
 end

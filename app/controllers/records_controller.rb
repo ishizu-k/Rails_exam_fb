480d8afaad719a44b6a25cdd5126aabafe8a6801
ishizu <demekinn_96@yahoo.co.jp>
2019-06-04 21:10:49 +0900

    records/index.html.erb（一覧表示画面）を作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,9 +1,21 @@
 class RecordsController < ApplicationController
   def index
+    @records = Record.all
   end
 
   def new
     @record = Record.new
+  end
+
+  def create
+    Record.create(record_params)
+    redirect_to records_path
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:content)
   end
 
 end

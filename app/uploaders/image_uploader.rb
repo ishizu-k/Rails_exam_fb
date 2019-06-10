8a93ff503d380055ed35868b12efc947a78d14d1
ishizu <demekinn_96@yahoo.co.jp>
2019-06-10 16:01:57 +0900

    デザインを微調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
 
-  process :resize_to_limit => [200, 200] # 画像サイズの調整
+  process :resize_to_limit => [500, 500] # 画像サイズの調整
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
